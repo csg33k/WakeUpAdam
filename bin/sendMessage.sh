8de9152585c3bf298c87a1f00ceae9ecaae93d9a
Samir Faci <samir@esamir.com>
2016-11-26 11:20:02 -0800

    Converting project to gradle build system

--- bin/sendMessage.sh
+++ bin/sendMessage.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 JAVA=$(which java)
-BASE=$(dirname $0)
-SCRIPTDIR=$BASE/..
-echo ${JAVA} -cp "${SCRIPTDIR}/target/wakeupAdam-1.0-SNAPSHOT.jar:${SCRIPTDIR}/target/lib/*" com.esamir.gvoice.Main
-${JAVA} -cp "${SCRIPTDIR}/target/wakeupAdam-1.0-SNAPSHOT.jar:${SCRIPTDIR}/target/lib/*" com.esamir.gvoice.Main
+echo ${JAVA} -jar build/lib/wakeupAdam-1.0-SNAPSHOT-all.jar
+${JAVA} ${JAVA} -jar build/lib/wakeupAdam-1.0-SNAPSHOT-all.jar
